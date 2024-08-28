de244dac74542e62ccb033884e7b51450c76e457
KavithRashintha <wgkrashintha@gmail.com>
2024-08-28 13:04:25 +0530

    Navbar modified

--- Front-End/vehicle_management_system/src/components/navigation-tabs/navigationTabs.js
+++ Front-End/vehicle_management_system/src/components/navigation-tabs/navigationTabs.js
@@ -43,18 +43,37 @@ function NavigationTabs() {
     };
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label="Car Dashboard" {...a11yProps(0)} />
-                    <Tab label="Rented Information" {...a11yProps(1)} />
-                </Tabs>
+        <Box sx={{ width: '100%', paddingTop: '8px', paddingBottom: '16px' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 2, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Box sx={{ flex: 1 }}>
+                    <span style={{ fontFamily: 'Inter', fontSize: '26px', color: '#22a6b3', fontWeight: 550, marginLeft: '24px', paddingTop: '14px' }}>
+                        Auto Travels
+                    </span>
+                </Box>
+                <Box sx={{ flex: 2, textAlign: 'right' }}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                        sx={{
+                            '& .MuiTab-root': {
+                                fontSize: '10px',  // Set font size
+                            },
+                            '& .MuiTabs-flexContainer': {
+                                justifyContent: 'flex-end', // Align tabs to the right
+                            },
+                        }}
+                    >
+                        <Tab label="Car Dashboard" {...a11yProps(0)} />
+                        <Tab label="Rented Information" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
             </Box>
             <CustomTabPanel value={value} index={0}>
-                <VehicleInformation></VehicleInformation>
+                <VehicleInformation />
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
-                <RentedInformation></RentedInformation>
+                <RentedInformation />
             </CustomTabPanel>
         </Box>
     );
